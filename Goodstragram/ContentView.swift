898d6b59b80fa146e6061fdbf7770cce9007cc44
Bagus Triyanto <44406611+bagusisgood@users.noreply.github.com>
2020-07-04 20:54:02 +0700

    Initial Commit

---
+++ Goodstragram/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Goodstragram
+//
+//  Created by Bagus Triyanto on 04/07/20.
+//  Copyright © 2020 Bagus Triyanto. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
